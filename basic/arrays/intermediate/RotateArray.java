dfabff88b2e56a253091244c33c6ea5ceb434737
Narendra Samtani <alenway97@gmail.com>
2025-09-15 20:38:38 +0530

    Rotate array at k position

---
+++ basic/arrays/intermediate/RotateArray.java
@@ -0,0 +1,11 @@
+public class RotateArray {
+    public static int[] rotate(int[] arr, int tilt) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        int tilt = 3;
+        int[] result = rotate(arr, tilt);
+    }
+}
