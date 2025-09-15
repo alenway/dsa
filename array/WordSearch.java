09ebaa62cda226382e4d8c6af036ded065e882a8
Narendra Samtani <alenway97@gmail.com>
2025-09-15 20:38:13 +0530

    word search in 2d char table

---
+++ array/WordSearch.java
@@ -0,0 +1,11 @@
+public class WordSearch {
+    public static void main(String[] args) {
+        char[][] board = {
+                { 'A', 'B', 'C', 'E' },
+                { 'S', 'F', 'C', 'S' },
+                { 'A', 'D', 'E', 'E' }
+        };
+        String word = "ABC";
+
+    }
+}
