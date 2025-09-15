5e57d767b736d100ef7506fbf9365f16813679b2
Narendra Samtani <alenway97@gmail.com>
2025-09-15 20:31:29 +0530

    linkedlist

---
+++ topics/linkedlist/LinkedListExp.java
@@ -0,0 +1,15 @@
+public class LinkedListExp {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
