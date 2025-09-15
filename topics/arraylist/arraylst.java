74b016b94a90a00983cf1b98cbecbaf11b882606
Narendra Samtani <alenway97@gmail.com>
2025-09-15 20:31:13 +0530

    arraylist

---
+++ topics/arraylist/arraylst.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+class arraylst {
+    public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+    }
+}
