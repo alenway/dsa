9490d974b1a85e1b49a699009a494e260b1b5d83
Narendra Samtani <alenway97@gmail.com>
2025-09-15 20:40:39 +0530

    find each vowel character frequency

---
+++ leetcode/hashTable/3541_FrequentVowel.java
@@ -0,0 +1,10 @@
+class FrequentVowels {
+    public static int frequency(String vowels) {
+        return 3;
+    }
+
+    public static void main(String[] args) {
+        String vowels = "aeiaeia";
+        int result = frequency(vowels);
+    }
+}
