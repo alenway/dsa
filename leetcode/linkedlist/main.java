fa780c0b8210145557354a3a9c69731e4a55a588
Narendra Samtani <alenway97@gmail.com>
2025-09-21 19:16:42 +0530

    linked list

---
+++ leetcode/linkedlist/main.java
@@ -0,0 +1,23 @@
+class main {
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    class LinkedList {
+        Node head;
+
+        LinkedList() {
+            head = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
