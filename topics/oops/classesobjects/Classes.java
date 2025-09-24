0804450c1ac0e7031f3029d83cfaafc353997c2b
Narendra Samtani <alenway97@gmail.com>
2025-09-25 01:15:33 +0530

    classes in java

--- topics/oops/classesobjects/Classes.java
+++ topics/oops/classesobjects/Classes.java
@@ -1,11 +1,27 @@
+// proper way to create a class
+class vehicle{
+    String name;
+    void show(){
+        System.out.println("this is vehicle class " + name );
+    }
+}
+// file name and main class
 public class Classes {
-    // creating class Car
-    class Car {
+    // creating class Car with static
+    static class Car extends vehicle{
         String color;
         int speed;
 
         void display() {
             System.out.println("Car is driving at " + speed + " km/h.");
+        }
+    }
+
+    // creating class inside a main class without static
+    class tata{
+        String name;
+        void moveTata(){
+            System.out.println("tata moves; " + name);
         }
     }
 
@@ -15,5 +31,14 @@ public class Classes {
         volvo.color = "Red";
         volvo.speed = 100;
         volvo.display();
+        vehicle v1 = new vehicle();
+        v1.name = "vehicle one";
+        v1.show();
+        volvo.show();
+
+        Classes outer = new Classes();
+        tata t = outer.new tata();
+        t.name = "Safari";
+        t.moveTata();
     }
 }
