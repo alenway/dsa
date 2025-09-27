b8266eeb04dc470bf3128e8004a3d0e6b0753dbc
Narendra Samtani <alenway97@gmail.com>
2025-09-27 23:53:14 +0530

    static methods in java

---
+++ topics/oops/01_foundations/04-staticInstance/staticMethods.java
@@ -0,0 +1,15 @@
+class MathUiLibrary {
+    public static double calculateRadius(double radius) {
+        return Math.PI * radius * radius;
+    }
+
+    public static void printInfo() {
+        System.out.println("something");
+    }
+}
+
+public class staticMethods {
+    public static void main(String[] args) {
+
+    }
+}
