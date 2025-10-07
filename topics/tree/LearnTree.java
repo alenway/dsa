e19c8ef774e64092776bedf7f9e194728b18532e
Narendra Samtani <alenway97@gmail.com>
2025-10-07 18:21:52 +0530

    data structure trees

---
+++ topics/tree/LearnTree.java
@@ -0,0 +1,23 @@
+class TreeNode {
+    public int value;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class LearnTree {
+    TreeNode root;
+
+    public LearnTree() {
+        root = null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
